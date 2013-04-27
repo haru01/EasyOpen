bdda347ac60d0e46263132619f4db6c3e870d125
eiji.ienaga <eiji.ienaga@gmail.com>
2013-04-27 14:20:59 +0900

    add search with github

--- browser_search.py
+++ browser_search.py
@@ -4,3 +4,8 @@ from helper import SearchWithBrowserCommand
 class SearchWithQaAtItCommand(SearchWithBrowserCommand):
     def url_search(self, text):
         return 'http://qa.atmarkit.co.jp/q/search?q=' + text.replace(' ', '%20')
+
+
+class SearchWithGithubCommand(SearchWithBrowserCommand):
+    def url_search(self, text):
+        return 'https://github.com/search?q=' + text.replace(' ', '%20')
