c4c148032a1fff7aa7bb6c3e6271f3531b14dab7
eiji.ienaga <eiji.ienaga@gmail.com>
2013-04-29 21:34:55 +0900

    add mdfind

---
+++ open_file_with_mdfind.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import sublime
+import sublime_plugin
+from helper import CommandExecutor
+
+
+class OpenFileWithMdfindCommand(sublime_plugin.WindowCommand, CommandExecutor):
+    force_open = False
+
+    def run(self):
+        self.window.show_input_panel('Mdfind:', '', self.on_done, None, None)
+
+    def on_done(self, input):
+        self.items = self.items_mdfind(input)
+        sublime.active_window().show_quick_panel(self.items, self.panel_done, sublime.MONOSPACE_FONT)
+
+    def items_mdfind(self, key):
+        results = self.run_cmd(['mdfind', key])
+        # TODO Error
+        return [item.decode('utf-8') for item in results['out'].split('\n') if item != '']
+
+    def panel_done(self, picked):
+        if 0 > picked < len(self.items):
+            return
+        picked_file = self.items[picked]
+        self.window.open_file(picked_file)
