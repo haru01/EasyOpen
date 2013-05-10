e7e0a86632a2ca5fab88d86bd041ef3546b81e63
eiji.ienaga <eiji.ienaga@gmail.com>
2013-05-10 23:44:56 +0900

    add index key

--- create_index.sh
+++ create_index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm .easyopen_index
 
-exp='(def\s|class\s|modlue\s)'
+exp='(def\s|class\s|modlue\s|\sattr_accessor\s|\sattr_reader\s|\sattr_accessor\s|\sscope\s|\sclass_attribute\s|\sbelongs_to\s|\shas_many\s|\sattr_readonly\s)'
 ag --noheading --nobreak --nocolor --line-numbers $exp . > .easyopen_index
 ag --noheading --nobreak --nocolor --line-numbers $exp  `bundle list --paths` >> .easyopen_index
